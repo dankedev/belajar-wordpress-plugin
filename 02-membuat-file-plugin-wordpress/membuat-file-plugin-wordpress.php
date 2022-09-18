273991b3b1337ff92f43b56e9c856a916362d61d
Hadie Danker <hadie87@gmail.com>
2022-09-19 03:22:18 +0700

    membuat plugin pertama kali

---
+++ 02-membuat-file-plugin-wordpress/membuat-file-plugin-wordpress.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: Plugin Pertama Yang saya buat
+ * Plugin URI : https://dankedev.com/plugins/plugin-saya
+ * Description: Belajar membuat plugin bersama hadie dankedev.com
+ * Version: 1.0.10
+ * Requires at least: 5.4
+ * Requires PHP: 8.0
+ * Author: Hadie Danker
+ * Author URI: https://www.dankedev.com
+* License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       dankedev
+ * Domain Path:       /languages
+ */
